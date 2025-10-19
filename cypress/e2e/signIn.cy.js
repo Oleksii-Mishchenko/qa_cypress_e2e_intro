683d47f5625394cf26e76b1a948c9030327fee6a
OleksiiMishchenko <alex.mishch@gmail.com>
2025-10-19 13:25:29 +0300

    Application login testing

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const username = 'user001';
+  const email = 'user001@hmail.com';
+  const password = 'Test1234';
 
+  it('should provide an ability to log in', () => {
+    cy.visit('/');
+
+    cy.contains('a', 'Sign in').click();
+
+    cy.url().should('equal', `${Cypress.config().baseUrl}/user/login`);
+
+    cy.get('input[placeholder="Email"]').type(email);
+
+    cy.get('input[placeholder="Password"]').type(password);
+
+    cy.contains('button[type="submit"]', 'Sign in').click();
+
+    cy.get('nav').contains('a', username).should('be.visible');
+
+    cy.url().should('equal', `${Cypress.config().baseUrl}/`);
   });
 });
